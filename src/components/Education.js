08473b93086496681ed98577488a28f86d0e5804
Ayush <webdeveloper.ayush@gmail.com>
2023-06-02 03:03:06 +0530

    scrool-of-experience

--- src/components/Education.js
+++ src/components/Education.js
@@ -42,8 +42,8 @@ const Education = () => {
         <div  ref={ref}className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
             <motion.div 
             style={{scaleY: scrollYProgress}}
-            className='absolute left-9 top-0 w-[4px]  h-full bg-dark  dark:bg-light origin-top md:w-[2px] md:left-[30px] xs:left-[20px]' />
-            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
+            className='absolute left-9 top-0 w-[4px]  h-full bg-dark  dark:bg-light origin-top md:w-[2px] md:left-[30px] xs:left-[28px]' />
+            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2.5'>
                 <Details
                 type="Higher Secondary Education (DPS Kalyanpur)" 
                 time="2016-2017" place= "Kanpur"
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -42,7 +42,7 @@ const Experience = () => {
             <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
                 <motion.div
                     style={{ scaleY: scrollYProgress }}
-                    className='absolute left-9 top-0 w-[4px]  h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]' />
+                    className='absolute left-9 top-0 w-[4px]  h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[28px]' />
                 <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2 '>
                     <Details
                         position="Frontend Engineer" company="SFC Foundations"
